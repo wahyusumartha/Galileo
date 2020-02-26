2b7e8750770eed0b83694619807555d0afe302c2
Wahyu Sumartha <w.priya-dharma@mytaxi.com>
2020-02-27 00:19:38 +0100

    Add `ServerValue` Enum

---
+++ Sources/Galileo/Firestore/Types/ServerValue.swift
@@ -0,0 +1,13 @@
+//
+//  ServerValue.swift
+//  Galileo
+//
+//  Created by Wahyu Sumartha Priya Dharma on 27.02.20.
+//
+
+import Foundation
+
+public enum ServerValue {
+	static let serverValueUnspecified = "SERVER_VALUE_UNSPECIFIED"
+	static let requestTime = "REQUEST_TIME"
+}
